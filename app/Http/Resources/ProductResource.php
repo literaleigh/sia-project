464ae628bd9a6ea135a616ee5ecb259460641090
aleya <aeliyaalfajora@gmail.com>
2024-12-15 12:34:59 +0800

    feat: Add created_at field to ProductResource for API response

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -19,6 +19,7 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
+            'created_at' => $this->created_at,
         ];
     }
 }
